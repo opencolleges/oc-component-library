315877ce2daf622126decf21467f3020d42dbf9d
Daniel Maddison <dmaddison@icloud.com>
2019-06-21 14:25:25 +1000

    Storybook UI updates.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,17 @@
+import { addParameters } from '@storybook/react';
+
+addParameters({
+  backgrounds: [
+    { name: 'Alabaster', value: '#f9f8f8', default: true },
+    { name: 'Charcoal', value: '#1c1c1c' },
+    { name: 'Viking tint', value: '#21d0d9' }
+  ]
+});
+
 import { configure } from '@storybook/react';
 
 import '../lib/all.scss';
+import './storybook.scss';
 
 const req = require.context('../lib', true, /\.stories\.(js|ts)$/);
 
