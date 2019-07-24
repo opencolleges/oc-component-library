fd9a9d0e2455c306d3f6dc8c10afadb9c94e7942
Saumil <saumil.shah@opencolleges.edu.au>
2019-07-24 10:36:02 +1000

    new ts file

---
+++ lib/index.ts
@@ -0,0 +1,80 @@
+import OCAccordion from './accordion/index.js';
+// import OCAvatar from './avatar';
+// import OCBadge from './badge';
+// import OCButton from './button';
+// import OCCard from './card';
+// import OCCheckbox from './checkbox';
+// import OCCheckboxSet from './checkbox-set';
+// import OCCopy from './copy';
+// import OCDate from './date';
+// import OCDivider from './divider';
+// import OCFile from './file';
+// import OCForm from './form';
+// import OCGrid from './grid';
+// import OCGridItem from './grid-item';
+// import OCHeading from './heading';
+// import OCIcon from './icon';
+// import OCLikert from './likert';
+// // import OCModal from './modal';
+// import OCPagination from './pagination';
+// import OCPreloader from './preloader';
+// import OCProgress from './progress';
+// import OCRadio from './radio';
+// import OCRadioSet from './radio-set';
+// // import OCRange from './range';
+// import OCSelect from './select';
+// import OCTable from './table';
+// import OCTableBody from './table-body';
+// import OCTableCell from './table-cell';
+// import OCTableHead from './table-head';
+// import OCTableRow from './table-row';
+// import OCText from './text';
+// import OCTextarea from './textarea';
+// import OCToast from './toast';
+// import OCToaster from './toaster';
+// import OCToggle from './toggle';
+// import OCUniform from './uniform';
+
+// import TestDND from './drag-drop/dragDropTest';
+// import DNDV from './drag-drop/dragDropV';
+
+export {
+  OCAccordion
+  // OCAvatar,
+  // OCBadge,
+  // OCButton,
+  // OCCard,
+  // OCCheckbox,
+  // OCCheckboxSet,
+  // OCCopy,
+  // OCDate,
+  // OCDivider,
+  // OCFile,
+  // OCForm,
+  // OCGrid,
+  // OCGridItem,
+  // OCHeading,
+  // OCIcon,
+  // OCLikert,
+  // // OCModal,
+  // OCPagination,
+  // OCPreloader,
+  // OCProgress,
+  // OCRadio,
+  // OCRadioSet,
+  // // OCRange,
+  // OCTable,
+  // OCTableBody,
+  // OCTableCell,
+  // OCTableHead,
+  // OCTableRow,
+  // OCText,
+  // OCTextarea,
+  // OCToast,
+  // OCToaster,
+  // OCToggle,
+  // OCSelect,
+  // OCUniform,
+  // DNDV,
+  // TestDND
+};
