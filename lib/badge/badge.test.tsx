5a47e989c02972e57b85e6401a4d3c95cbda68ff
Daniel Maddison <dmaddison@icloud.com>
2019-07-30 14:36:31 +1000

    [TSD-2766] Badge unit tests

--- lib/badge/badge.test.tsx
+++ lib/badge/badge.test.tsx
@@ -2,55 +2,59 @@ import * as React from 'react';
 
 import Badge from './badge';
 
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 describe(`<Badge />`, () => {
-  it(`Default`, () => {
-    const badge = shallow(<Badge />);
+  let badge;
+  beforeEach(() => {
+    badge = mount(<Badge />);
+  });
 
-    expect(badge.find(`.oc-badge`).length).toBe(1);
-    expect(badge.find(`span`).text()).toBe(`0`);
+  afterEach(() => {
+    badge.unmount();
   });
 
   it(`Handles props.modifiers`, () => {
-    const badge1 = shallow(<Badge modifiers="badge--error" />);
-    const badge2 = shallow(<Badge modifiers="badge--success" />);
+    expect(badge.getDOMNode().getAttribute(`class`)).toBe(`oc-badge`);
 
-    expect(badge1.find(`.oc-badge--error`).length).toBe(1);
-    expect(badge2.find(`.oc-badge--success`).length).toBe(1);
+    badge.setProps({ modifiers: `badge--error` });
+    expect(badge.getDOMNode().getAttribute(`class`)).toBe(
+      `oc-badge oc-badge--error`
+    );
+
+    badge.setProps({ modifiers: `badge--success` });
+    expect(badge.getDOMNode().getAttribute(`class`)).toBe(
+      `oc-badge oc-badge--success`
+    );
   });
 
   it(`Handles props.className`, () => {
-    const badge = shallow(
-      <Badge modifiers="badge--error" className="foo bar" />
-    );
+    expect(badge.getDOMNode().getAttribute(`class`)).toBe(`oc-badge`);
 
-    expect(badge.find(`.oc-badge--error`).length).toBe(1);
-    expect(badge.find(`.foo`).length).toBe(1);
-    expect(badge.find(`.bar`).length).toBe(1);
+    badge.setProps({ className: `foo` });
+    expect(badge.getDOMNode().getAttribute(`class`)).toBe(`oc-badge foo`);
+
+    badge.setProps({ className: `foo bar` });
+    expect(badge.getDOMNode().getAttribute(`class`)).toBe(`oc-badge foo bar`);
   });
 
   it(`Handles props.style`, () => {
-    const badge = shallow(
-      <Badge
-        modifiers="badge--success"
-        className="baz"
-        style={{ zIndex: `1` }}
-      />
-    );
+    expect(badge.getDOMNode().getAttribute(`style`)).toBe(null);
 
-    expect(badge.find(`.oc-badge--success`).length).toBe(1);
-    expect(badge.find(`.baz`).length).toBe(1);
-    expect(badge.html()).toContain(`style="z-index:1"`);
+    badge.setProps({ style: { zIndex: `1` } });
+    expect(badge.getDOMNode().getAttribute(`style`)).toBe(`z-index: 1;`);
   });
 
   it(`Handles props.value`, () => {
-    const badge1 = shallow(<Badge value="16 qux" />);
-    const badge2 = shallow(<Badge value="10" />);
-    const badge3 = shallow(<Badge value={160} />);
+    expect(badge.find(`span`).text()).toBe(`0`);
 
-    expect(badge1.find(`span`).text()).toBe(`16 qux`);
-    expect(badge2.find(`span`).text()).toBe(`9+`);
-    expect(badge3.find(`span`).text()).toBe(`99+`);
+    badge.setProps({ value: '16 qux' });
+    expect(badge.find(`span`).text()).toBe(`16 qux`);
+
+    badge.setProps({ value: '10' });
+    expect(badge.find(`span`).text()).toBe(`9+`);
+
+    badge.setProps({ value: 160 });
+    expect(badge.find(`span`).text()).toBe(`99+`);
   });
 });
