761db9b3e6a0f5224ddf061d23cfc060060fc6d0
Daniel Maddison <dmaddison@icloud.com>
2019-08-01 09:36:24 +1000

    <Uniform /> bugFix

--- lib/uniform/uniform.tsx
+++ lib/uniform/uniform.tsx
@@ -1,4 +1,4 @@
-import * as detectIt from 'detect-it';
+import detectIt from 'detect-it';
 import * as React from 'react';
 
 import namespace from '../utilities/js/namespace';
@@ -10,8 +10,9 @@ const Uniform: React.FC<IUniformProps> = props => {
   const Tag: keyof JSX.IntrinsicElements = props.tag;
   const theme: string = getUniformTheme();
   const device: string = detectIt.hasMouse ? 'no-touchevents' : '';
-  const class_names: string = `${namespace(theme, device)}${props.className &&
-    ` ${props.className}`}`;
+  const class_names: string = `${namespace(theme, device)}${
+    props.className ? ` ${props.className}` : ''
+  }`;
 
   return (
     <Tag className={class_names} style={props.style}>
