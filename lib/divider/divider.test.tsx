50ac8d161296ee21b1ff296995be7a4a43440936
Daniel Maddison <dmaddison@icloud.com>
2019-08-07 14:58:03 +1000

    TSD-2786 unit testing

---
+++ lib/divider/divider.test.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+
+import Divider from './divider';
+
+import { mount } from 'enzyme';
+
+let wrapper;
+
+describe('<Divider />', () => {
+  beforeEach(() => {
+    wrapper = mount(<Divider />);
+  });
+
+  it('Handles props.modifiers', () => {
+    expect(wrapper.getDOMNode().getAttribute('class')).toBe(
+      'oc-divider oc-divider--s'
+    );
+
+    wrapper.setProps({ modifiers: 'divider--m' });
+    expect(wrapper.getDOMNode().getAttribute('class')).toBe(
+      'oc-divider oc-divider--m'
+    );
+
+    wrapper.setProps({ modifiers: 'divider--l' });
+    expect(wrapper.getDOMNode().getAttribute('class')).toBe(
+      'oc-divider oc-divider--l'
+    );
+  });
+
+  it('Handles props.className', () => {
+    expect(wrapper.getDOMNode().getAttribute('class')).toBe(
+      'oc-divider oc-divider--s'
+    );
+
+    wrapper.setProps({ className: 'foo' });
+    expect(wrapper.getDOMNode().getAttribute('class')).toBe(
+      'oc-divider oc-divider--s foo'
+    );
+
+    wrapper.setProps({ className: 'bar' });
+    expect(wrapper.getDOMNode().getAttribute('class')).toBe(
+      'oc-divider oc-divider--s bar'
+    );
+  });
+
+  it('Handles props.style', () => {
+    expect(wrapper.getDOMNode().getAttribute('style')).toBe(null);
+
+    wrapper.setProps({ style: { zIndex: '1' } });
+    expect(wrapper.getDOMNode().getAttribute('style')).toBe('z-index: 1;');
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+});
